6eeb6aee025702807d8aca7d2076b50ce71f2ef4
Lizbeth-Laptop <115133403+WillAvatec@users.noreply.github.com>
2024-07-31 11:25:31 -0500

    new UserRepository

---
+++ src/main/java/com/agroapp/agroapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.agroapp.agroapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.agroapp.agroapp.Entity.User;
+
+public interface UserRepository extends CrudRepository<User,Long>{
+    User findByUsername(String username);
+}
