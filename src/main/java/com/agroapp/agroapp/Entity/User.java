9ab1246483c4b527fef23cd6fa891a326affe73e
Lizbeth-Laptop <115133403+WillAvatec@users.noreply.github.com>
2024-07-31 11:25:17 -0500

    feat: Include relation of Roles to Users

--- src/main/java/com/agroapp/agroapp/Entity/User.java
+++ src/main/java/com/agroapp/agroapp/Entity/User.java
@@ -1,9 +1,15 @@
 package com.agroapp.agroapp.Entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +21,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity
 
-public class User {
+public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,4 +34,13 @@ public class User {
     private String role;
     private String zipCode;
 
+
+    // Relationships
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "users_roles", 
+        joinColumns = @JoinColumn(name = "user_id"), 
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
 }
